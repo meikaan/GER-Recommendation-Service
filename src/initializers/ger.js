0f61334751fb6d331b639b18051fa74214850e87
sharanchavadi <sharanschavadi@gmail.com>
2019-03-12 23:36:51 +0530

    created namespace_config file and initialized namespaces

--- src/initializers/ger.js
+++ src/initializers/ger.js
@@ -1,33 +1,23 @@
 'use strict';
 
 const g = require('ger');
-//const pg = require('pg');
+const namespaces = require('../assets/namespace_config.json');
 
 module.exports = function() {
 
   const app = this;
 
-  /*let knex = g.knex( {client: 'pg', connection: {
-	    host : 'localhost',
-	    port: 5432,
-	    user : 'postgres',
-	    password : '',
-	    database : 'ger'
-	  	//"postgres://postgres@localhost/ger"
-	  }
-	});*/
   //TODO: const preferred, check the impact of const and change it accordingly
-
   let knex = g.knex( {client: 'pg', connection: "postgres://apple:123@localhost/mosaic"});
   let esm = new g.PsqlESM({knex : knex});
   let ger = new g.GER(esm);
 
-  //let esm = new g.MemESM();
-  //let ger = new g.GER(esm);
-
   app.set("ger", ger);
 
-  ger.initialize_namespace('activities').then(() => {
-  	console.log("ger namespace initialized");
-  })
+  namespaces.forEach( namespace => {
+    ger.initialize_namespace(namespace['name']).then(() => {
+      console.log(`ger namespace - ${namespace['name']} initialized`);
+    });  
+  });
+
 };
