0ac4d3a47735669a56c68f5537eddfbbc77e0e92
sharanchavadi <sharanschavadi@gmail.com>
2019-03-12 17:28:55 +0530

    fixed postgreSQL connection issue

--- src/initializers/ger.js
+++ src/initializers/ger.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const g = require('ger')
+const g = require('ger');
+//const pg = require('pg');
 
 module.exports = function() {
 
@@ -16,8 +17,13 @@ module.exports = function() {
 	  }
 	});*/
   //TODO: const preferred, check the impact of const and change it accordingly
-  let esm = new g.MemESM();
+
+  let knex = g.knex( {client: 'pg', connection: "postgres://apple:123@localhost/mosaic"});
+  let esm = new g.PsqlESM({knex : knex});
   let ger = new g.GER(esm);
+
+  //let esm = new g.MemESM();
+  //let ger = new g.GER(esm);
 
   app.set("ger", ger);
 
