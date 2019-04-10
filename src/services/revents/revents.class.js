1726d5a437a2fde91153a5e62330c53871e91d09
sharanchavadi <sharanschavadi@gmail.com>
2019-04-10 19:47:33 +0530

    Setting up default expires_at for events

--- src/services/revents/revents.class.js
+++ src/services/revents/revents.class.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+var moment = require('moment');
+
 class Service {
   constructor (options) {
     this.options = options || {};
@@ -10,26 +12,37 @@ class Service {
   }
 
   async get (id, params) {
+    
     return {
       id, text: `A new message with ID: ${id}!`
     };
+
   }
 
   async create (data, params) {
-    console.log("---reached revents---");
     //TODO: Find better way to ensure mandatory params
-    const mandatoryParams = ['namespace', 'person', 'action', 'thing', 'expires_at'];
+    const mandatoryParams = ['namespace', 'person', 'action', 'thing'];
+
     if(data.constructor !== Array){
       data = [data];
     }
+
     data.forEach(event => {
       mandatoryParams.forEach(param => {
         if(!!event[param])return;
         throw new Error(`Missing Parameter: ${param}`); 
       })  
     })
+
+    data.forEach(event => {
+      if (!event.expires_at){
+        event.expires_at = moment().add(1, 'y').format('YYYY-MM-DD');
+      }
+    })
+    
     console.log(data);
-    return this.app.get('ger').events(data);  
+    return this.app.get('ger').events(data);
+
   }
 
   async update (id, data, params) {
